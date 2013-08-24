2bbda3ad4a159045fd2eb781e20d77f6aac6af73
Trent Small <Sixstring982@gmail.com>
2013-08-23 22:38:51 -0600

    Added AssetManager. Added Updating and Rendering to Game1.cs . Currently broken, as there are no Screens in the ScreenStack to process.

---
+++ WolfSim/WolfSim/WolfSim/AssMan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WolfSim
+{
+    enum IAsset
+    {
+
+    }
+
+    class AssMan
+    {
+        public static Dictionary<IAsset, Texture2D> IDict = new Dictionary<IAsset, Texture2D>();
+
+        public static void LoadStaticAssets(ContentManager c)
+        {
+            //Code here looks like this:
+            // IDict.Add(IAsset.ASSET_NAME_HERE, c.Load<Texture2D>("ASSET_STRING_FROM_CONTENT_HERE"));
+        }
+
+        public static Texture2D Get(IAsset a)
+        {
+            return IDict[a];
+        }
+    }
+}
---
+++ WolfSim/WolfSim/WolfSim/KVMA_Keyboard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WolfSim
+{
+    class KVMA_Keyboard
+    {
+        private static KeyboardState current = Keyboard.GetState(),
+                                     prev = Keyboard.GetState();
+
+        public static bool KeyDown(Keys k)
+        {
+            return current.IsKeyDown(k);
+        }
+
+        public static bool KeyUp(Keys k)
+        {
+            return current.IsKeyUp(k);
+        }
+
+        public static bool SemiAuto(Keys k)
+        {
+            return current.IsKeyDown(k) && !prev.IsKeyDown(k);
+        }
+
+        public static void Flip()
+        {
+            prev = current;
+            current = Keyboard.GetState();
+        }
+    }
+}
